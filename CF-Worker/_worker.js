fc4f79becaafa0bad74060fe384f3ce9a2aa344b
sjzkn1 <70833700+sjzkn1@users.noreply.github.com>
2026-01-20 08:11:16 +0800

    Update _worker.js

--- CF-Worker/_worker.js
+++ CF-Worker/_worker.js
@@ -6,7 +6,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let 我的VL密钥 = '04c808e2-0b59-47b0-a54b-32fc7ef1c902';//UUID
+let 我的VL密钥 = 'fb8bd75b-dde7-4c64-8fe8-ac2ed21dbe8f';//UUID
 let 反代IP = 'proxyip.cmliussss.net'; //反代IP
 
 export default {
